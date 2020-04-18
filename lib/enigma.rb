2e0948ad71dd2bc53e5bafd4bd0b924683a145e8
Derek Borski <derek.borski@gmail.com>
2020-04-18 17:01:32 -0600

    Adds character_set and shift_values attributes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,10 @@
+require './lib/shift'
+
 class Enigma
 
+  attr_reader :character_set, :shift_values
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+    @shift_values = Shift.new.create_shift
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/enigma'
+require './lib/shift'
 
 class EnigmaTest < Minitest::Test
 
@@ -9,5 +10,17 @@ class EnigmaTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_character_set
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, @enigma.character_set
+  end
+
+  def test_shift_values
+    assert_instance_of Hash, @enigma.shift_values
+    assert_equal 6, @enigma.shift_values.length
   end
 end
