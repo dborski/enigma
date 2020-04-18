8aa087f5ea3c23bd513fd6a50f3e8f4c7e1e2d41
Derek Borski <derek.borski@gmail.com>
2020-04-18 10:27:58 -0600

    Adds generate_random_num method

--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,11 @@
 class Key
 
+  attr_reader :number
   def initialize(number)
     @number = number
+  end
+
+  def generate_random_num
+    rand(0..99999).to_s.rjust(5, "0")
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -10,4 +10,16 @@ class KeyTest < Minitest::Test
   def test_it_exists
     assert_instance_of Key, @key
   end
+
+  def test_has_readable_attributes
+    assert_equal "01234", @key.number
+  end
+
+  def test_can_generate_random_number
+
+    num = @key.generate_random_num
+
+    assert_equal true, num.to_i < 100000
+    assert_equal 5, @key.generate_random_num.length
+  end
 end
