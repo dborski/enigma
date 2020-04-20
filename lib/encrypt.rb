7ae03cbb53f9c1c31d709e3cbda0b61f6ce18712
Derek Borski <derek.borski@gmail.com>
2020-04-20 15:08:57 -0600

    Adds write functionality to encrypted.txt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,7 +6,9 @@ chopped_message = message.chomp
 text.close
 
 encrypted_message = Enigma.new.encrypt(chopped_message)
+message_to_write = encrypted_message[:encryption]
+writer = File.open(ARGV[1], "w")
+writer.write(message_to_write)
+writer.close
 
-require "pry"; binding.pry
-# writer = File.open(ARGV[1], "w")
-# writer.close
+# require "pry"; binding.pry
