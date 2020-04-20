8a687df8c831ef08ef0ca667c36b22446946fa85
Derek Borski <derek.borski@gmail.com>
2020-04-20 15:02:28 -0600

    Pulls in encrypted message from Enigma class

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,3 +1,12 @@
-message = File.open(ARGV[0], "r")
-text = message.read
-message.close
+require_relative 'enigma'
+
+text = File.open(ARGV[0], "r")
+message = text.read
+chopped_message = message.chomp
+text.close
+
+encrypted_message = Enigma.new.encrypt(chopped_message)
+
+require "pry"; binding.pry
+# writer = File.open(ARGV[1], "w")
+# writer.close
