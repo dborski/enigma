59e7fd0860e9e05ee954423276edfe207ed2a910
Derek Borski <derek.borski@gmail.com>
2020-04-20 21:13:21 -0600

    Creates new instance of Enigma instead of typing Enigma.new.encrypt/decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,6 +1,8 @@
 require_relative 'enigma'
 require 'date'
 
+@enigma = Enigma.new
+
 text = File.open(ARGV[0], "r")
 message = text.read
 chopped_message = message.chomp
@@ -8,7 +10,7 @@ text.close
 
 key = ARGV[2]
 
-decrypted_message = Enigma.new.decrypt(chopped_message, key)
+decrypted_message = @enigma.decrypt(chopped_message, key)
 message_to_write = decrypted_message[:decryption]
 writer = File.open(ARGV[1], "w")
 writer.write(message_to_write)
